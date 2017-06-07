096f27da11c83aabb58f5ed5a2c998a55ffaf8e9
cagataycali <cagataycali@icloud.com>
2017-06-07 20:32:59 +0300

    📦  Cool improvements

--- index.js
+++ index.js
@@ -1,114 +1,111 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const shortid = require('shortid');
 const emoji = require('./emoji');
 const fetch = str => emoji(str).then(arr => arr.slice(0, 8).join(''));
 const mongoose = require('mongoose');
 const punycode = require('punycode');
+const subdomain = require('subdomain');
+const check = require('ch3ck');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
+app.use(subdomain({ base : 'localhost', removeWWW : true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
 var Schema = new mongoose.Schema({
   key: { type: String, unique: true, required: true },
   url: { type: String, unique: true, required: true },
 });
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
 var mongolabUri = process.env.MONGODB_URI;
-mongoose.connect(mongolabUri, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
+mongoose.connect(mongolabUri, err => console.log(err ? err : 'Mongo connected.'));
 
-var Keys = mongoose.model('Keys', Schema);
+var Keys = mongoose.model('Sites', Schema);
 
-
-
-app.get('/:key?', (req, res) => {
-  console.log('DOMAIN',req.headers.host);
-  console.log(req.params.key, req.params.key ? true : false);
-  if (req.params.key) {
-    Keys.findOne({ 'key': req.params.key }, function (err, key) {
-      if (!err && key) {
-        res.redirect(301, key.url)
+const find = (key) => {
+  return new Promise((resolve, reject) => {
+    console.log(key);
+    Keys.findOne({ 'key': key }, (err, key) => {
+      console.log(key);
+      if (key && key.url.length > 0) {
+        resolve(key);
       } else {
-        console.log('Fallback home', err, key);
-        res.redirect(301, 'http://🌍✌🏼.ws')
+        reject(true);
       }
     });
-  } else {
-    res.send(`
-      <html>
-        <head>
-          <title>🌍✌🏼</title>
-          <meta name="image" content="https://emojipedia-us.s3.amazonaws.com/cache/52/23/52230e8ab5779d3b28c53725672bad99.png">
-          <link rel="icon" href="/static/favicon.ico">
-          <!-- Schema.org for Google -->
-          <meta itemprop="name" content="🌍✌🏼">
-          <meta itemprop="description" content="Shitty url shortener, emoji and ai powered. 🌍✌🏼">
-          <meta itemprop="image" content="https://emojipedia-us.s3.amazonaws.com/cache/52/23/52230e8ab5779d3b28c53725672bad99.png">
-          <!-- Twitter -->
-          <meta name="twitter:card" content="summary">
-          <meta name="twitter:title" content="🌍✌🏼">
-          <meta name="twitter:description" content="Shitty url shortener, emoji and ai powered.">
-          <meta name="twitter:site" content="@cagataycali">
-          <meta name="twitter:creator" content="@cagataycali">
-          <!-- Open Graph general (Facebook, Pinterest & Google+) -->
-          <meta name="og:title" content="🌍✌🏼">
-          <meta name="og:description" content="Shitty url shortener, emoji and ai powered.">
-          <meta name="og:image" content="https://emojipedia-us.s3.amazonaws.com/cache/52/23/52230e8ab5779d3b28c53725672bad99.png">
-          <meta name="og:url" content="http://🌍✌🏼.ws">
-          <meta name="og:site_name" content="🌍✌🏼">
-          <meta name="og:locale" content="en_US">
-          <meta name="og:type" content="website">
-        </head>
-        <body>
-          Home, sweet home... 🏡<br>
-          I guess you have a shitty URL. That's why you're here. 💩 <br>
-          Let's see what I can do. <br><br>
-          <form method="post" action="/">
-            <input type="text" name="url" placeholder="Your shitty URL." required><br>
-            Emoji ? <input type="checkbox" name="emoji" value="true" checked>
-            <input type="submit" value="FUCK IT">
-          </form>
-          <br>
-          No reporting, no follow-up. It just works. <br>
-          The world is ours, become a contributor. 🌍✌🏼<br>
-          <a href="https://github.com/cagataycali/url-shortener">GitHub</a>
-        </body>
-       </html>
-      `)
+  });
+}
+
+app.get('/check/:key', async (req, res) => {
+  try {
+    let response = await find(req.params.key);
+      response.message = `Aww, ${res.url} grabbed it first.!`
+      res.json(response);
+  } catch (e) {
+    if (req.params.key.length === 2) {
+      res.json('If you want to use this emoji, you should open an issue.')
+    } else {
+      res.json('Available for purchase! Haha, It\'s joke. But contributions is welcome.')
+    }
   }
 });
+
+app.get('/subdomain/:key/', async (req, res) => {
+  try {
+    req.params.key = punycode.toUnicode(req.params.key);
+    const response = await find(req.params.key);
+    console.log('REDIRECT', response);
+    res.redirect(301, response.url);
+  } catch (e) {
+    res.json({message:'Available for purchase! Haha, It\'s joke. But contributions is welcome.'});
+  }
+});
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.get('/:key', async (req, res) => {
+  try {
+    const response = await find(req.params.key);
+    console.log('REDIRECT', response);
+    res.redirect(301, response.url);
+  } catch (e) {
+    res.json({message:'Available for purchase! Haha, It\'s joke. But contributions is welcome.'});
+  }
+})
 
 app.post('/', async (req, res) => {
-  let key = req.body.emoji ? await fetch(req.body.url) : shortid.generate();
-  if (!req.body.url.includes('http')) {
-    req.body.url = 'http://' + req.body.url;
-  }
-  var keys = new Keys({
-    url : req.body.url,
-    key,
-  });
-  res.json({
-    "💩": punycode.toUnicode(req.body.url),
-    "😻": `${punycode.toUnicode(req.headers.host)}/${key}`,
-    "message": "Do not do bad things to the server. Contributions welcome. http://🌍✌🏼.ws/♥☎💻❤"
-  });
-  keys.save(function (err, doc) {
-    if (err && err.code === 11000 && !req.body.emoji) {
-      Keys.update({ url: req.body.url }, { $set: { key: key }}, (err) => {
-        console.log(err ? err : 'Updated');
-      });
+  if (check(req.body.url) !== false) {
+    let url = check(req.body.url);
+    console.log(url);
+    let emoji = req.body.emoji;
+    console.log('length',emoji.length);
+
+    if (emoji.length === 2) {
+      res.json({status:false, message: 'If you want to use this emoji, you should open an issue.'})
+    } else {
+      let keys = new Keys({
+       url,
+       key: emoji,
+     });
+
+     keys.save((err, doc) => {
+       if (err) {
+         res.json({status:false, message: 'Somebody grabbed your emoji or you already shortened your url.'})
+       } else {
+         io.sockets.emit('new', `${emoji} grabbed by ${url} now.`)
+         res.json({status:true, url: `http://${req.headers.host}/${emoji}`, subdomain: `http://${emoji}.${req.headers.host}`})
+       }
+     });
     }
-  });
+ } else {
+   res.json({status:false, message: 'Gimme valid url.'})
+ }
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(`Example app listening on port ${process.env.PORT || 3000}!`)
-})
+http.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${process.env.PORT || 3000}!`));
